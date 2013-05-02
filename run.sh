a56fe3854f4261e8669a0ad69c1641b06bdfe9e0
David <admin@dav1d.de>
2013-05-02 15:33:15 +0200

    Improve run-script

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-kill $(ps ax | grep "SimpleHTTPServer 8000" | grep -v grep | awk '{ print $1 }')
-python2 -m SimpleHTTPServer 8000 2>&1 > http.log&
-./watch.sh 2>&1 > watch.log
+PID=$(ps ax | grep "SimpleHTTPServer 8000" | grep -v grep | awk '{ print $1 }')
+
+if [ "$PID" != "" ]; then
+    echo -n "Kill running SimpleHTTPServer on port 8000? [y/n]: "
+    read KILL
+    if [ "$KILL" == "y" -o "$KILL" == "yes" ]; then
+        kill $PID
+    else
+        exit
+    fi
+fi
+
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
+
+./watch.sh 2>&1 | tee watch.log &
+python2 -m SimpleHTTPServer 8000 1>http.log 2>&1
