0ad5a6fd8f0b0e7833ed779dbb4e7b20e6546a57
David <admin@dav1d.de>
2013-05-05 23:27:40 +0200

    include offline-version in packed archive

--- pack.sh
+++ pack.sh
@@ -7,6 +7,11 @@ cp -r docroot/ /tmp/pack_m16/m16/
 cp -r dep/ /tmp/pack_m16/m16/
 sed -i -e "s/\.\/dep/\/dep/" /tmp/pack_m16/m16/index.html
 
+pushd /tmp/pack_m16/ >/dev/null
+zip -r m16.zip m16/ >/dev/null
+mv m16.zip m16/
+popd >/dev/null
+
 tar -pczf m16.tar.gz -C /tmp/pack_m16/ m16/
 
 rm -rf /tmp/pack_m16/
